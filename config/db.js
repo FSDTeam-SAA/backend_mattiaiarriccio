e877523f2b932ce58512a4dc7a8a33511648115b
Fahim <ahmed.fahim37@gmail.com>
2026-05-14 14:28:03 +0600

    updates

--- config/db.js
+++ config/db.js
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose';
+import { ensurePromptConfigIndexes } from '../models/promptConfig.model.js';
 
 const DEFAULT_URI = 'mongodb://127.0.0.1:27017/wesafe';
+let indexesReady = false;
 
 export const getMongoUri = () => process.env.MONGODB_URI || DEFAULT_URI;
 
 export const connectToDatabase = async () => {
-  if (mongoose.connection.readyState === 1) {
-    return mongoose.connection;
+  if (mongoose.connection.readyState !== 1) {
+    await mongoose.connect(getMongoUri());
   }
 
-  await mongoose.connect(getMongoUri());
+  if (!indexesReady) {
+    await ensurePromptConfigIndexes();
+    indexesReady = true;
+  }
+
   return mongoose.connection;
 };
 
--- models/promptConfig.model.js
+++ models/promptConfig.model.js
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 
+const DEFAULT_PROMPT_TYPE = 'global_prompt';
+const DEFAULT_PROMPT_LANGUAGE = 'en';
+const PROMPT_CONFIG_UNIQUE_INDEX = 'type_1_language_1';
+const PROMPT_TEXT_FIELDS = [
+  'welcome_instruction',
+  'system_instruction',
+  'fallback_message'
+];
+
 const promptConfigSchema = new mongoose.Schema(
   {
     type: {
@@ -24,10 +33,82 @@ const promptConfigSchema = new mongoose.Schema(
   }
 );
 
-promptConfigSchema.index({ type: 1, language: 1 }, { unique: true });
+promptConfigSchema.index(
+  { type: 1, language: 1 },
+  { unique: true, name: PROMPT_CONFIG_UNIQUE_INDEX }
+);
 
 const PromptConfig =
   mongoose.models.PromptConfig ||
   mongoose.model('PromptConfig', promptConfigSchema);
+
+const isSingleTypeUniqueIndex = (index) => {
+  const keys = Object.keys(index.key || {});
+  return index.unique === true && keys.length === 1 && index.key.type === 1;
+};
+
+const migratePromptConfigLanguage = async () => {
+  const missingLanguageDocs = await PromptConfig.find({
+    $or: [
+      { language: { $exists: false } },
+      { language: null },
+      { language: '' }
+    ]
+  })
+    .sort({ updated_at: -1 })
+    .lean();
+
+  for (const doc of missingLanguageDocs) {
+    const type = doc.type || DEFAULT_PROMPT_TYPE;
+    const existing = await PromptConfig.findOne({
+      _id: { $ne: doc._id },
+      type,
+      language: DEFAULT_PROMPT_LANGUAGE
+    });
+
+    if (!existing) {
+      await PromptConfig.updateOne(
+        { _id: doc._id },
+        {
+          $set: {
+            type,
+            language: DEFAULT_PROMPT_LANGUAGE
+          }
+        }
+      );
+      continue;
+    }
+
+    const merge = {};
+    for (const field of PROMPT_TEXT_FIELDS) {
+      if (!existing[field] && doc[field]) {
+        merge[field] = doc[field];
+      }
+    }
+
+    if (Object.keys(merge).length > 0) {
+      await PromptConfig.updateOne({ _id: existing._id }, { $set: merge });
+    }
+
+    await PromptConfig.deleteOne({ _id: doc._id });
+  }
+};
+
+export const ensurePromptConfigIndexes = async () => {
+  await PromptConfig.createCollection();
+  await migratePromptConfigLanguage();
+
+  const indexes = await PromptConfig.collection.indexes();
+  for (const index of indexes) {
+    if (isSingleTypeUniqueIndex(index)) {
+      await PromptConfig.collection.dropIndex(index.name);
+    }
+  }
+
+  await PromptConfig.collection.createIndex(
+    { type: 1, language: 1 },
+    { unique: true, name: PROMPT_CONFIG_UNIQUE_INDEX }
+  );
+};
 
 export default PromptConfig;
